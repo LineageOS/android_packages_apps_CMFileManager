318f278b0a69fd6e4830f0b72cff869c1d8b50c9
Danny Baumann <dannybaumann@web.de>
2013-01-03 16:48:13 +0100

    Fall back to showing all files for unknown mime types.
    It's better to let the user select among all files than not presenting
    him any files at all.
    
    Patchset 2: simplify the logic
    
    Change-Id: I372f00cccce1c9bb69d5d2668bb33c7882580e08

--- src/com/cyanogenmod/filemanager/util/MimeTypeHelper.java
+++ src/com/cyanogenmod/filemanager/util/MimeTypeHelper.java
@@ -192,6 +192,22 @@ public final class MimeTypeHelper {
     }
 
     /**
+     * Method that checks whether a certain mime type is known to
+     * the application.
+     *
+     * @param context The current context
+     * @param mimeType The mime type to be checked
+     * @return true if mime type is known, false otherwise
+     */
+    public static final boolean isMimeTypeKnown(Context context, String mimeType) {
+        //Ensure that mime types are loaded
+        if (sMimeTypes == null) {
+            loadMimeTypes(context);
+        }
+        return sMimeTypes.containsKey(mimeType);
+    }
+
+    /**
      * Method that returns the associated mime/type icon resource identifier of
      * the {@link FileSystemObject}.
      *
