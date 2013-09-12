e8cf7200cff85d2b54d6fef16a257f0198665891
Jo De Boeck <deboeck.jo@gmail.com>
2013-09-12 21:47:44 +0200

    Implement showing of apk icon in listing
    Change-Id: I0a4ea801ff0cfbf31dda125b8925e7d35f4c3c99

--- src/com/cyanogenmod/filemanager/ui/IconHolder.java
+++ src/com/cyanogenmod/filemanager/ui/IconHolder.java
@@ -17,9 +17,16 @@
 package com.cyanogenmod.filemanager.ui;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
+import android.util.Log;
 
+import com.cyanogenmod.filemanager.model.FileSystemObject;
 import com.cyanogenmod.filemanager.ui.ThemeManager.Theme;
+import com.cyanogenmod.filemanager.util.MimeTypeHelper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -59,5 +66,27 @@ public class IconHolder {
         this.mIcons.put(resid, dw);
         return dw;
     }
+
+    public Drawable getDrawable(Context context, FileSystemObject fso) {
+        String filepath = fso.getFullPath();
+        String mime = MimeTypeHelper.getMimeType(context, fso);
+        if (TextUtils.equals(mime , "application/vnd.android.package-archive")) {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo packageInfo = pm.getPackageArchiveInfo(filepath,
+                                           PackageManager.GET_ACTIVITIES);
+            if (packageInfo != null) {
+                ApplicationInfo appInfo = packageInfo.applicationInfo;
+                appInfo.sourceDir = filepath;
+                appInfo.publicSourceDir = filepath;
+                Drawable dw = pm.getDrawable(appInfo.packageName, appInfo.icon, appInfo);
+                if (dw != null) {
+                    return dw;
+                }
+            }
+        }
+        String resid = MimeTypeHelper.getIcon(context, fso);
+        return getDrawable(context, resid);
+    }
+
 
 }
