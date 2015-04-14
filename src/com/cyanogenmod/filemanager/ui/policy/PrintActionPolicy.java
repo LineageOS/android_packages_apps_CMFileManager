25912b0fb3398eaae565390515a238add415b556
jing.zhao <jing.zhao@ck-telecom.com>
2015-04-14 15:21:38 +0800

    CMFileManager: Printing preview content is not show complete.
    fix the PageCount from int to double
    
    Change-Id: I72f1577f22b563456b43dded4058572d328009c3

--- src/com/cyanogenmod/filemanager/ui/policy/PrintActionPolicy.java
+++ src/com/cyanogenmod/filemanager/ui/policy/PrintActionPolicy.java
@@ -351,8 +351,8 @@ public final class PrintActionPolicy extends ActionsPolicy {
         }
 
         private int calculatePageCount(int rowsPerPage) {
-            int pages = mAdjustedLines.size() / rowsPerPage;
-            return pages <= 0 ? PrintDocumentInfo.PAGE_COUNT_UNKNOWN : pages;
+            double pages = (double)mAdjustedLines.size() / rowsPerPage;
+            return pages <= 0 ? PrintDocumentInfo.PAGE_COUNT_UNKNOWN : ((Double)Math.ceil(pages)).intValue();
         }
 
         private int rowsPerPage(Rect pageContentRect) {
