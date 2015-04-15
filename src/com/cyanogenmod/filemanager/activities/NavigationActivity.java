da776f07a44307f77758bab77254bbbbd0a6b90a
Martin Brabham <optedoblivion@cyngn.com>
2015-04-15 15:33:25 -0700

    Implement a dialog that warns the user that we must expose the content of the file by copying it out to an unsecure location in order to allow the external applications to read the files.
    Change-Id: I163ccd21678f413170e44cf3e8d341cd4747b1ac
    (cherry picked from commit 58928e7facbdd63d4320748b277e94417fe402bb)

--- src/com/cyanogenmod/filemanager/activities/NavigationActivity.java
+++ src/com/cyanogenmod/filemanager/activities/NavigationActivity.java
@@ -1945,6 +1945,11 @@ public class NavigationActivity extends Activity
         // Ignored
     }
 
+    @Override
+    public void onCancel(){
+        // nop
+    }
+
     /**
      * {@inheritDoc}
      */
