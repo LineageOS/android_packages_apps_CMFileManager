e2ff00c7c30e32bcbe6fdfc6944c75172f312755
Stephen Bird <sebirdman@gmail.com>
2015-05-12 17:30:16 -0700

    CM Easy Mode File Manager Navigation fixes
    This patch fixes an issue where the user cannot get back to easy mode without exiting and re-entering the application in certain situations
    
    Change-Id: I3a995358ca7e2905fa5de05ec9584fb3b4c7343a

--- src/com/cyanogenmod/filemanager/activities/NavigationActivity.java
+++ src/com/cyanogenmod/filemanager/activities/NavigationActivity.java
@@ -471,6 +471,8 @@ public class NavigationActivity extends Activity
 
     private int mOrientation;
 
+    private boolean mNeedsEasyMode = false;
+
     /**
      * @hide
      */
@@ -492,6 +494,7 @@ public class NavigationActivity extends Activity
         if (DEBUG) {
             Log.d(TAG, "NavigationActivity.onCreate"); //$NON-NLS-1$
         }
+
 
         // Register the broadcast receiver
         IntentFilter filter = new IntentFilter();
@@ -681,6 +684,16 @@ public class NavigationActivity extends Activity
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
           return true;
+        }
+
+        if (mNeedsEasyMode) {
+            if (item.getItemId() == android.R.id.home) {
+                if (mHistory.size() == 0 && !isEasyModeVisible()) {
+                    performShowEasyMode();
+                } else {
+                    back();
+                }
+            }
         }
         return super.onOptionsItemSelected(item);
     }
@@ -968,6 +981,7 @@ public class NavigationActivity extends Activity
     private void performShowEasyMode() {
         mEasyModeListView.setVisibility(View.VISIBLE);
         getCurrentNavigationView().setVisibility(View.GONE);
+        performShowBackArrow(false);
     }
 
     /**
@@ -976,6 +990,20 @@ public class NavigationActivity extends Activity
     private void performHideEasyMode() {
         mEasyModeListView.setVisibility(View.GONE);
         getCurrentNavigationView().setVisibility(View.VISIBLE);
+    }
+
+    private void performShowBackArrow(boolean showBackArrow) {
+        if (mNeedsEasyMode) {
+            mDrawerToggle.setDrawerIndicatorEnabled(!showBackArrow);
+        }
+    }
+
+    private boolean isEasyModeVisible() {
+        if (mEasyModeListView.getVisibility() == View.GONE) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     /**
@@ -1084,6 +1112,7 @@ public class NavigationActivity extends Activity
                         } else {
                             performHideEasyMode();
                         }
+                        performShowBackArrow(false);
                         getCurrentNavigationView().open(fso);
                         mDrawerLayout.closeDrawer(Gravity.START);
                     }
@@ -1459,7 +1488,9 @@ public class NavigationActivity extends Activity
         intent.putExtra(SearchManager.QUERY, "*"); // Use wild-card '*'
 
         if (position == 0) {
+            // the user has selected all items, they want to see their folders so let's do that.
             performHideEasyMode();
+            performShowBackArrow(true);
             return;
 
         } else {
@@ -1657,8 +1688,10 @@ public class NavigationActivity extends Activity
             }
         }
 
+        mNeedsEasyMode = getResources().getBoolean(R.bool.cmcc_show_easy_mode);
+
         needsEasyMode = needsEasyMode
-                && getResources().getBoolean(R.bool.cmcc_show_easy_mode);
+                && mNeedsEasyMode;
         if (needsEasyMode) {
             performShowEasyMode();
         } else {
@@ -1748,6 +1781,13 @@ public class NavigationActivity extends Activity
         if (checkBackAction()) {
             performHideEasyMode();
             return;
+        } else {
+            if (mNeedsEasyMode) {
+                if (!isEasyModeVisible()) {
+                    performShowEasyMode();
+                    return;
+                }
+            }
         }
 
         // An exit event has occurred, force the destroy the consoles
@@ -2134,7 +2174,11 @@ public class NavigationActivity extends Activity
             //Communicate the user that the next time the application will be closed
             this.mExitBackTimeout = System.currentTimeMillis();
             DialogHelper.showToast(this, R.string.msgs_push_again_to_exit, Toast.LENGTH_SHORT);
-            return true;
+            if (mNeedsEasyMode) {
+                return isEasyModeVisible();
+            } else {
+                return true;
+            }
         }
 
         //Back action not applied
