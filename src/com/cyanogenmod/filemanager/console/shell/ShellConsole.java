d33d916001ab590910225202e0770f1be0c40668
tobitege <tobiasteschner@googlemail.com>
2015-06-23 11:24:52 +0200

    Avoid ArrayIndexOutOfBoundsException in isCommandFinished()
    The partial.write always expects 2 bytes in match, which is not
    checked for actual size.
    
    Log excerpt:
    W/ShellConsole( 5971): java.lang.ArrayIndexOutOfBoundsException: length=53; regionStart=5; regionLength=53
    W/ShellConsole( 5971): 	at java.util.Arrays.checkOffsetAndCount(Arrays.java:1719)
    W/ShellConsole( 5971): 	at java.io.ByteArrayOutputStream.write(ByteArrayOutputStream.java:197)
    W/ShellConsole( 5971): 	at com.cyanogenmod.filemanager.console.shell.ShellConsole.isCommandFinished(ShellConsole.java:1239)
    W/ShellConsole( 5971): 	at com.cyanogenmod.filemanager.console.shell.ShellConsole$2.run(ShellConsole.java:913)
    (with vanilla CMF 12.1 repo)
    
    Change-Id: I62cc60e3248a2ba2bc4ebed8adb47c7fb3585142

--- src/com/cyanogenmod/filemanager/console/shell/ShellConsole.java
+++ src/com/cyanogenmod/filemanager/console/shell/ShellConsole.java
@@ -1236,7 +1236,9 @@ public abstract class ShellConsole extends Console implements Program.ProgramLis
 
             if (match != null) {
                 partial.reset();
-                partial.write(bytes, match[0], match[1]);
+                if (match.length == 2) {
+                    partial.write(bytes, match[0], match[1]);
+                }
             }
         }
         return ret;
