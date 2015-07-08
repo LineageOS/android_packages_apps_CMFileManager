b4d6b6ec45f4f3310220816a9ea85ac41b3b5995
Stephen Bird <sbird@cyngn.com>
2015-07-08 13:03:32 -0700

    Easy Mode: Fix exit on easy mode screen
    Change-Id: Iec39763194916c12c1947fb4f297e74c6dfa2f5f

--- src/com/cyanogenmod/filemanager/activities/NavigationActivity.java
+++ src/com/cyanogenmod/filemanager/activities/NavigationActivity.java
@@ -1801,12 +1801,14 @@ public class NavigationActivity extends Activity
             mDrawerLayout.closeDrawer(Gravity.START);
             return;
         }
-        if (checkBackAction()) {
-            performHideEasyMode();
+
+        boolean upToParent = mHistory.size() > 0;
+
+        if (mNeedsEasyMode && !isEasyModeVisible() && !upToParent) {
+            performShowEasyMode();
             return;
         } else {
-            if (mNeedsEasyMode && !isEasyModeVisible()) {
-                performShowEasyMode();
+            if (checkBackAction()) {
                 return;
             }
         }
