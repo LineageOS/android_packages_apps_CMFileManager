383538f8276690ba272d5ff740ad021ad5c95ca8
Stephen Bird <sbird@cyngn.com>
2015-07-09 16:28:20 -0700

    Secure Storage: Fix unlock button
    Change-Id: I852fff76aef4f47bbd6d809c64afcc2f77165553
    (cherry picked from commit c2832885bd00d45ac7277930cff68063e1cb4d3a)

--- src/com/cyanogenmod/filemanager/console/secure/SecureStorageKeyPromptDialog.java
+++ src/com/cyanogenmod/filemanager/console/secure/SecureStorageKeyPromptDialog.java
@@ -245,19 +245,19 @@ public class SecureStorageKeyPromptDialog
             boolean validLength = key.length() >= MIN_PASSWORD_LENGTH &&
                     (!mResetPassword || (mResetPassword && oldkey.length() >= MIN_PASSWORD_LENGTH));
             boolean validEquals = key.equals(repeatKey);
-            boolean valid = validLength &&
-                    (((mNewStorage || mResetPassword) && validEquals) || mDeleteStorage);
-            mUnlock.setEnabled(valid);
 
             if (!validLength) {
                 mValidationMsg.setText(getString(R.string.secure_storage_unlock_validation_length,
                         MIN_PASSWORD_LENGTH));
                 mValidationMsg.setVisibility(View.VISIBLE);
+                mUnlock.setEnabled(false);
             } else if ((mNewStorage || mResetPassword) && !validEquals) {
                 mValidationMsg.setText(R.string.secure_storage_unlock_validation_equals);
                 mValidationMsg.setVisibility(View.VISIBLE);
+                mUnlock.setEnabled(false);
             } else {
                 mValidationMsg.setVisibility(View.INVISIBLE);
+                mUnlock.setEnabled(true);
             }
         }
 
