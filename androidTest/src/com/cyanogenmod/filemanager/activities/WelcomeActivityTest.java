7f21fdec1140d13d77cc6c3bcae42d546bbf2ce1
weldoncyngn <wng@cyngn.com>
2015-07-09 16:36:45 -0700

    Initial commit for Espresso tests.
    Change-Id: Ie2cd998d9399d51ad646d7ffe3e82cf9ca61a6b7

---
+++ androidTest/src/com/cyanogenmod/filemanager/activities/WelcomeActivityTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.filemanager.activities;
+
+import com.cyanogenmod.filemanager.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.app.Activity;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.TextView;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+
+/**
+ * Basic tests showcasing simple view matchers and actions like {@link ViewMatchers#withId},
+ * {@link ViewActions#click} and {@link ViewActions#typeText}.
+ * <p>
+ * Note that there is no need to tell Espresso that a view is in a different {@link Activity}.
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class WelcomeActivityTest {
+
+    public static final String STRING_TO_BE_TYPED = "Espresso";
+
+    /**
+     * A JUnit {@link Rule @Rule} to launch your activity under test. This is a replacement
+     * for {@link ActivityInstrumentationTestCase2}.
+     * <p>
+     * Rules are interceptors which are executed for each test method and will run before
+     * any of your setup code in the {@link Before @Before} method.
+     * <p>
+     * {@link ActivityTestRule} will create and launch of the activity for you and also expose
+     * the activity under test. To get a reference to the activity you can use
+     * the {@link ActivityTestRule#getActivity()} method.
+     */
+    @Rule
+    public ActivityTestRule<WelcomeActivity> mActivityRule = new ActivityTestRule<>(
+            WelcomeActivity.class);
+
+    @Test
+    public void testClickNextOnWelcomePage() {
+        onView(withText("Welcome to")).check(matches(isDisplayed()));
+        onView(withId(R.id.prevButton)).check(matches(withEffectiveVisibility(Visibility.INVISIBLE)));
+        onView(withId(R.id.nextButton)).perform(click());
+    }
+}
