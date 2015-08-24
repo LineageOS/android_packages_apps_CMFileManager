4721d56b67965543f3ff03dad207e71ae15dfbca
Roman Birg <roman@cyngn.com>
2015-08-24 09:42:47 -0700

    CMFM: don't apply search filter when search hasn't started
    Otherwise it can lead to crashes with short searches
    
    Ref: QRDL-950
    
    Change-Id: Icf555435b4cc7107db328c757633a29ffacd62c5
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/cyanogenmod/filemanager/activities/SearchActivity.java
+++ src/com/cyanogenmod/filemanager/activities/SearchActivity.java
@@ -1518,7 +1518,9 @@ public class SearchActivity extends Activity
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        new SearchResultFilterTask().execute(MimeTypeHelper.MimeTypeCategory.values()[i]);
+        if (mResultList != null && !mResultList.isEmpty()) {
+            new SearchResultFilterTask().execute(MimeTypeHelper.MimeTypeCategory.values()[i]);
+        }
     }
 
     @Override
