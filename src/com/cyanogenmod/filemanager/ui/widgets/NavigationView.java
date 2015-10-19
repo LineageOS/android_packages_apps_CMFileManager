def8397f85c46ed6d24d1bd1ffd23dfc2f6300f6
root <srinivas.p031@gmail.com>
2015-10-19 19:24:28 +0530

    Thumbnail update on copy and overwrite issue of pictures
    Change-Id: I10d3c861141590806a8e5b9c665b4f4f4b74141a

--- src/com/cyanogenmod/filemanager/ui/widgets/NavigationView.java
+++ src/com/cyanogenmod/filemanager/ui/widgets/NavigationView.java
@@ -829,7 +829,9 @@ BreadcrumbListener, OnSelectionChangedListener, OnSelectionListener, OnRequestRe
             this.mCurrentDir = this.mPreviousDir;
             this.mPreviousDir = null;
         }
-
+        if (this.mAdapter != null) {
+            mAdapter.clearCache(scrollTo);
+        }
         //Reload data
         changeCurrentDir(newDir, addToHistory, reload, useCurrent, searchInfo, scrollTo);
     }
