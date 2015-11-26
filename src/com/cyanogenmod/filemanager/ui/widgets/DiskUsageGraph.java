59c9f1911dab44b95fa60a18cfeed6d0264a2f2e
Fujia Mi <fmi@codeaurora.org>
2015-11-26 14:27:37 +0800

    CMFileManager: Refresh the disk usage graph when layout size change
    The size of disk usage graph was caculated dynamically according to
    the parent layout size. When the device was rotated from landscape
    to portrait, the layout size was changed, but the disk usage graph
    did not refresh.
    
    Redraw the graph when the view size changes.
    
    Change-Id: I2e283ed77f44b759f4f3753ff92fe95d851b6433
    CRs-Fixed: 889743

--- src/com/cyanogenmod/filemanager/ui/widgets/DiskUsageGraph.java
+++ src/com/cyanogenmod/filemanager/ui/widgets/DiskUsageGraph.java
@@ -77,6 +77,8 @@ public class DiskUsageGraph extends View {
         }
     };
 
+    private DiskUsage mLastDiskUsage;
+
     /**
      * Initialize the color assets into memory for direct access
      */
@@ -156,6 +158,13 @@ public class DiskUsageGraph extends View {
         this.setMeasuredDimension(size, size);
     }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        // Redraw the disk usage graph when layout size changes
+        if (oldw != 0 && oldh != 0 && mLastDiskUsage != null) {
+            drawDiskUsage(mLastDiskUsage);
+        }
+    }
     /**
      * Method that sets the free disk space percentage after the widget change his color to advise
      * the user
@@ -191,6 +200,7 @@ public class DiskUsageGraph extends View {
         // Start drawing thread
         AnimationDrawingRunnable animationDrawingRunnable = new AnimationDrawingRunnable(diskUsage);
         mAnimationFuture = sThreadPool.submit(animationDrawingRunnable);
+        mLastDiskUsage = diskUsage;
 
     }
 
