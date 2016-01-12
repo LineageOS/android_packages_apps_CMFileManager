e6fccfa96f6196a5a8f4299407f0b1b674e39771
yongga <yongga@codeaurora.org>
2016-01-12 14:23:17 +0800

    CMFileManager: Do not create multiple action dialogs
    Multiple instances of ActionsDialog are created if the user
    clicks the menu too quickly.
    
    Do not create an instance if a dialog has already been displayed.
    
    CRs-Fixed: 960229, 970649
    Change-Id: I610331cf71cb1b59aef6f89d8fdcb1c6ccda6044

--- src/com/cyanogenmod/filemanager/ui/dialogs/ActionsDialog.java
+++ src/com/cyanogenmod/filemanager/ui/dialogs/ActionsDialog.java
@@ -199,6 +199,16 @@ public class ActionsDialog implements OnItemClickListener, OnItemLongClickListen
     }
 
     /**
+     * Method that judges whether the dialog is showing.
+     */
+    public boolean isShowing() {
+        if (this.mDialog != null) {
+            return this.mDialog.isShowing();
+        }
+        return false;
+    }
+
+    /**
      * {@inheritDoc}
      */
     @Override
