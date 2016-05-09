825c86e9e7628ee88c14d6d09eed47a84b3201da
Kamaljeet Maini <kmaini@cyngn.com>
2016-05-09 14:12:43 -0700

    Refresh icons view when switching between portrait and landscape
    When switching between portrait and landscape modes, the icons view
    needs to be updated to re-arrange the icons based on updated width.
    
    Added a new method for refreshing icons view mode. This method is
    called during orientation changes.
    
    Issue-Id: FEIJ-492
    
    Change-Id: I9c95357a7d4f57b223147e774838e4fdc19f5e89

--- src/com/cyanogenmod/filemanager/ui/widgets/NavigationView.java
+++ src/com/cyanogenmod/filemanager/ui/widgets/NavigationView.java
@@ -843,6 +843,18 @@ BreadcrumbListener, OnSelectionChangedListener, OnSelectionListener, OnRequestRe
     }
 
     /**
+     * Method that refreshes the Icons layout mode.
+     * This is currently called for refreshing Icons layout mode when switching between portrait
+     * and landscape. Other layout modes don't need to be refreshed due to list view display
+     */
+    public void refreshViewMode() {
+        if (this.mCurrentMode.compareTo(NavigationLayoutMode.ICONS) == 0) {
+            this.mCurrentMode = null;
+            changeViewMode(NavigationLayoutMode.ICONS);
+        }
+    }
+
+    /**
      * Method that change the view mode.
      *
      * @param newMode The new mode
