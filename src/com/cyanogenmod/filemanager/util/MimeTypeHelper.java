676a6fb58033c52937be9fe1f533744bbeee2d2c
Keith Mok <kmok@cyngn.com>
2016-06-13 09:45:23 -0700

    Filter out vnd.android.cursor mimetype for Picker
    We don't support vnd.android.cursor mimetype,
    the return result is never be expected to caller.
    Ideally, we should filter that mimetype in AndroidManifest.xml,
    however we currently accept */* mimetype andd there is no
    way to exclude a specific mimetype.
    
    FEIJ-1138 FEIJ-1140 FEIJ-1141
    
    Change-Id: I407159c5f5283e4d5cec2ec129a7643723bc8602

--- src/com/cyanogenmod/filemanager/util/MimeTypeHelper.java
+++ src/com/cyanogenmod/filemanager/util/MimeTypeHelper.java
@@ -227,6 +227,17 @@ public final class MimeTypeHelper {
     }
 
     /**
+      * Method that checks whether it is a special android cursor mimetype
+      * that we don't support and should filter it all
+      *
+      * @param mimeType The mime type to be checked
+      * @return true if mime type is known, false otherwise
+      */
+     public static final boolean isAndroidCursorMimeType(String mimeType) {
+         return mimeType.startsWith("vnd.android.cursor");
+     }
+
+    /**
      * Method that checks whether a certain mime type is known to
      * the application.
      *
